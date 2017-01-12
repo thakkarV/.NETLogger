24fffe62e35ad1d466db9473fbd5ba53c91f4909
Vijay Thakkar <vijaythakkar@icloud.com>
2017-01-12 12:54:08 +0530

    Derp - forgot the test code last commit
    Here is the test code from last commit.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NETLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int i = 0; i < 1000; ++i)
+            {
+                Logger.Instance.debug("Test " + i);
+            }
+
+            try
+            {
+                FileStream fs = new FileStream(@"C:\temp.txt", FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.exception("Thrown from main.", e);
+            }
+        }
+    }
+}
